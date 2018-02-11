9778e6313f504cec730ba4c71dc97eaa96f3162a
dd84ai <dd84ai@gmail.com>
2018-02-11 15:44:20 +0700

    Default SharpGL C# App

--- TheDiplomWork/Program.cs
+++ TheDiplomWork/Program.cs
@@ -16,7 +16,7 @@ namespace TheDiplomWork
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SharpGLForm());
         }
     }
 }
