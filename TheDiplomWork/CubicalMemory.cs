770c99c9c8317a4d56cda4912cbf7d870a214746
dd84ai <dd84ai@gmail.com>
2018-02-13 00:17:04 +0700

    Кубы и Чанки. Алгоритмический базис плоской земли. След вопросы по жесткой памяти. Заметка по представлению чанков. С учетом того что их можно обнулять в полный ноль, мы можем себя не ограничивать по их количеству и спокойно писать добрую тысячу. Заметка: Garbage Collector.

---
+++ TheDiplomWork/CubicalMemory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDiplomWork
+{
+    class CubicalMemory : GeneralProgrammingStuff
+    { 
+        class Cube
+        {
+            double rangeOfTheEdge = 1.0;
+            System.Drawing.Color color = System.Drawing.Color.Gray;
+
+            public bool IsFilled = false;
+        }
+
+        class Chunk
+        {
+            const int Height = 32, Width = 16, Length = 16;
+
+            Cube[][][] cubes;
+
+            public Chunk()
+            {
+                TripleDimIniter<Cube>(ref cubes, Chunk.Height, Chunk.Width, Chunk.Length);
+
+                int FromHeight = 0, ToHeight = 10;
+                AlgorithmicalGround(FromHeight, ToHeight);
+            }
+
+            /// <summary>
+            /// Basically it's the Algorithm to display default non-hard-memored earth ground.
+            /// You can make it more advanced for Plants, Craters and e.t.c.
+            /// </summary>
+            /// <param name="fromHeight"></param>
+            /// <param name="toHeight"></param>
+            void AlgorithmicalGround(int fromHeight, int toHeight)
+            {
+                for (int i = fromHeight; i < toHeight; i++)
+                    for (int j = 0; j < Width; j++)
+                        for (int k = 0; k < Length; k++)
+                            cubes[i][j][k].IsFilled = true;
+            }
+        }
+
+            //Кубик состоит из 6 граней, квадратов.
+        }
+}
---
+++ TheDiplomWork/GeneralProgrammingStuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDiplomWork
+{
+    class GeneralProgrammingStuff
+    {
+        /// <summary>
+        /// I'm too lazy to write down this function for every thing I need
+        /// to initialize something in three dimensions.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="subject"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        public static void TripleDimIniter<T>(ref T[][][] subject, int a, int b, int c)
+        {
+            subject = new T[a][][];
+            for (int i = 0; i < a; i++)
+            {
+                subject[i] = new T[b][];
+                for (int j = 0; j < b; j++)
+                    subject[i][j] = new T[c];
+            }
+        }
+    }
+    
+}
