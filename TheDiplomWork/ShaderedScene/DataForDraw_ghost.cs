23598e6754fb8b71fbcbfba94793330fc75bbbfc
Andrey Novoselov <dd84ai@gmail.com>
2018-04-19 11:41:08 +0700

    Визуальный спецэффект норм.

--- TheDiplomWork/ShaderedScene/DataForDraw_ghost.cs
+++ TheDiplomWork/ShaderedScene/DataForDraw_ghost.cs
@@ -11,17 +11,18 @@ namespace TheDiplomWork
         public override void initialization()
         {
             START_initialization();
-
+            bool thing = false;
             bool found = false;
             try
             {
-                bool thing = Scene.SS.env.cub_mem.world.World_as_Whole
+                thing = Scene.SS.env.cub_mem.world.World_as_Whole
                             [Scene.SS.env.player.coords.Player_chunk_lookforcube.x]
                             [Scene.SS.env.player.coords.Player_chunk_lookforcube.z].cubes
                             [Scene.SS.env.player.coords.Player_cubical_lookforcube.x]
                             [Scene.SS.env.player.coords.Player_cubical_lookforcube.y]
                             [Scene.SS.env.player.coords.Player_cubical_lookforcube.z].IsFilled;
                 found = true;
+                
             }
             catch (Exception e)
             {
@@ -30,10 +31,29 @@ namespace TheDiplomWork
 
             if (found)
             {
+                System.Drawing.Color result = System.Drawing.Color.Gray;
+                if (thing)
+                {
+                    System.Drawing.Color colour = Scene.SS.env.cub_mem.world.World_as_Whole
+                                [Scene.SS.env.player.coords.Player_chunk_lookforcube.x]
+                                [Scene.SS.env.player.coords.Player_chunk_lookforcube.z].cubes
+                                [Scene.SS.env.player.coords.Player_cubical_lookforcube.x]
+                                [Scene.SS.env.player.coords.Player_cubical_lookforcube.y]
+                                [Scene.SS.env.player.coords.Player_cubical_lookforcube.z].color;
+
+                    result = System.Drawing.Color.FromArgb
+                        (
+                        (int)((float)(GraphicalOverlap.GO_color.R + colour.R) / 2),
+                        (int)((float)(GraphicalOverlap.GO_color.G + colour.G) / 2),
+                        (int)((float)(GraphicalOverlap.GO_color.B + colour.B) / 2)
+                        );
+                }
+                else result = GraphicalOverlap.GO_color;
+
                 ShaderedScene.CalculateFromMaptoGraphical(Scene.SS.env.player.coords.Player_chunk_lookforcube,
                     Scene.SS.env.player.coords.Player_cubical_lookforcube, ref x, ref y, ref z);
 
-                Draw_Quad_Full_Sunsided(x, y, z, localed_range, GraphicalOverlap.GO_color, true);
+                Draw_Quad_Full_Sunsided(x, y, z, localed_range, result, true);
 
             }
 
