60d2c6530caf6c6a094edfd06e9ff4973ab64ade
dd84ai <dd84ai@gmail.com>
2018-04-28 21:07:00 +0700

    На секондари отключил поинтофвью

--- TheDiplomWork/Scene/Scene.cs
+++ TheDiplomWork/Scene/Scene.cs
@@ -362,7 +362,7 @@ namespace TheDiplomWork
 
                 sunMatrix = new mat3(new vec3(-(float)Time.time.GetTotalRadianTime(), 0, 0),
                     new vec3(0, DataForDraw.localed_range * Sun.LocalSun.Sun_Height, 0),//new vec3(0, (float)+DataForDraw.localed_range * 100, 0),
-                    new vec3(StaticSettings.S.SunStatus.x, 0, 1.0f));
+                    new vec3(StaticSettings.S.SunStatus.x, 1.0f, 1.0f));
                 shaderProgram_secondary.SetUniformMatrix3(gl, "sunMatrix", sunMatrix.to_array());
 
                 SI_ghost.vertexBufferArray.Bind(gl);
