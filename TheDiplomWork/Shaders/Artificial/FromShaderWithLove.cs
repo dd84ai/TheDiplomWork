a68afaa0712c504b6865099f2995c32feddfd9ce
Andrey Novoselov <dd84ai@gmail.com>
2018-05-17 17:24:17 +0700

    W

---
+++ TheDiplomWork/Shaders/Artificial/FromShaderWithLove.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpGL;
+using GlmNet;
+namespace TheDiplomWork
+{
+    public class FromShaderWithLove
+    {
+        public class ShaderRotator
+        {
+            vec3 in_Position;
+            public ShaderRotator(vec3 inp)
+            {
+                in_Position = inp;
+            }
+            vec3 Rotate(vec3 SomethingToRotate, vec3 ang)
+            {
+                mat3 RotateX = new mat3(new vec3(1, 0, 0),
+                new vec3(0, (float)Math.Cos(ang.x), -(float)Math.Sin(ang.x)),
+                new vec3(0, (float)Math.Sin(ang.x), (float)Math.Cos(ang.x)));
+
+                mat3 RotateY = new mat3(new vec3((float)Math.Cos(ang.y), 0, (float)Math.Sin(ang.y)),
+                new vec3(0, 1, 0),
+                new vec3(-(float)Math.Sin(ang.y), 0, (float)Math.Cos(ang.y)));
+
+                mat3 RotateZ = new mat3(new vec3((float)Math.Cos(ang.z), -(float)Math.Sin(ang.z), 0),
+                new vec3((float)Math.Sin(ang.z), (float)Math.Cos(ang.z), 0),
+                new vec3(0, 0, 1));
+                mat3 Rotator = RotateX * (RotateY * (RotateZ));
+
+                vec3 temp = Rotator * SomethingToRotate;
+                return temp;
+            }
+            vec3 Shifted(vec3 input_vec)
+            {
+                return new vec3(input_vec + (in_Position));
+            }
+            vec3 Deshifted(vec3 input_vec)
+            {
+                return new vec3(input_vec - (in_Position));
+            }
+            vec3 ProcessingProjectile(vec3 inp)
+            {
+                //Rotate
+                vec3 ShiftToRotate = Shifted(inp) - Projectile.jp.center;
+                ShiftToRotate = Rotate(ShiftToRotate, Projectile.jp.PlayerAngles());
+                //ShiftToRotate = Deshifted(ShiftToRotate) + Projectile.jp.center;
+
+                vec3 ShiftedToRelativePosition = Projectile.jp.Coordinates() + ShiftToRotate;
+
+                return ShiftedToRelativePosition;
+            }
+            public vec3 ReturnTheThing() { return ProcessingProjectile(new vec3(0, 0, 0)); }
+        }
+    }
+}
--- TheDiplomWork/WorldObjects/Projectile.cs
+++ TheDiplomWork/WorldObjects/Projectile.cs
@@ -54,7 +54,7 @@ namespace TheDiplomWork
                 }
             }
             vec3 angles = new vec3(0, 0, 0);
-            vec3 PlayerAngles()
+            public vec3 PlayerAngles()
             {
                 if (RotatingStartingVelocity)
                 {
@@ -186,7 +186,7 @@ namespace TheDiplomWork
             }
             public void ProcessStartingData()
             {
-                ShaderRotator SR = new ShaderRotator(center + sd.default_velocity);
+                FromShaderWithLove.ShaderRotator SR = new FromShaderWithLove.ShaderRotator(center + sd.default_velocity);
                 if (!Launched)
                 {
                     sd.starting_velocity = SR.ReturnTheThing();//Rotate(sd.default_velocity, PlayerAngles());
@@ -202,52 +202,7 @@ namespace TheDiplomWork
                 Launched = false;
             }
 
-            class ShaderRotator
-            {
-                vec3 in_Position;
-                public ShaderRotator(vec3 inp)
-                {
-                    in_Position = inp;
-                }
-                vec3 Rotate(vec3 SomethingToRotate, vec3 ang)
-                {
-                    mat3 RotateX = new mat3(new vec3(1, 0, 0),
-                    new vec3(0, (float)Math.Cos(ang.x), -(float)Math.Sin(ang.x)),
-                    new vec3(0, (float)Math.Sin(ang.x), (float)Math.Cos(ang.x)));
-
-                    mat3 RotateY = new mat3(new vec3((float)Math.Cos(ang.y), 0, (float)Math.Sin(ang.y)),
-                    new vec3(0, 1, 0),
-                    new vec3(-(float)Math.Sin(ang.y), 0, (float)Math.Cos(ang.y)));
-
-                    mat3 RotateZ = new mat3(new vec3((float)Math.Cos(ang.z), -(float)Math.Sin(ang.z), 0),
-                    new vec3((float)Math.Sin(ang.z), (float)Math.Cos(ang.z), 0),
-                    new vec3(0, 0, 1));
-                    mat3 Rotator = RotateX * (RotateY * (RotateZ));
-
-                    vec3 temp = Rotator * SomethingToRotate;
-                    return temp;
-                }
-                vec3 Shifted(vec3 input_vec)
-                {
-                    return new vec3(input_vec + (in_Position));
-                }
-                vec3 Deshifted(vec3 input_vec)
-                {
-                    return new vec3(input_vec - (in_Position));
-                }
-                vec3 ProcessingProjectile(vec3 inp)
-                {
-                    //Rotate
-                    vec3 ShiftToRotate = Shifted(inp) - Projectile.jp.center;
-                    ShiftToRotate = Rotate(ShiftToRotate, Projectile.jp.PlayerAngles());
-                    //ShiftToRotate = Deshifted(ShiftToRotate) + Projectile.jp.center;
-
-                    vec3 ShiftedToRelativePosition = Projectile.jp.Coordinates() + ShiftToRotate;
-
-                    return ShiftedToRelativePosition;
-                }
-                public vec3 ReturnTheThing() { return ProcessingProjectile(new vec3(0, 0, 0)); }
-            }
+            
 
             vec3 os_x = new vec3(1, 0, 0);
             vec3 os_y = new vec3(1, 0, 0);
